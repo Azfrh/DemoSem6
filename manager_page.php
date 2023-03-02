74b882551786bd7eb133e8b72b8b6774b7dbfd47
Azfrh <2020897568@student.uitm.edu.my>
2023-03-03 06:28:58 +0800

    manager_page

---
+++ manager_page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>manager page</title>
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<div class="container">
+
+    <div class="content">
+        <h3>hi, <span>manager</span></h3>
+        <h1>welcome <span></span></h1>
+        <p>this is an manager page</p>
+        <a href="login_form.php"class="btn">login</a>
+        <a href="register_form.php"class="btn">register</a>
+        <a href="logout_form.php"class="btn">logout</a>
+    </div>
+
+</div>
+     
+
+</body>
+</html>
