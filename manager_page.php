39db308e72b5cba55fc9e0060209c69bea2210b6
Azfrh <2020897568@student.uitm.edu.my>
2023-03-03 07:38:19 +0800

    manager.page

--- manager_page.php
+++ manager_page.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>manager page</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
 
-    <!-- custom css file link -->
-    <link rel="stylesheet" href="css/style.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-    
+   
 <div class="container">
 
-    <div class="content">
-        <h3>hi, <span>manager</span></h3>
-        <h1>welcome <span></span></h1>
-        <p>this is an manager page</p>
-        <a href="login_form.php"class="btn">login</a>
-        <a href="register_form.php"class="btn">register</a>
-        <a href="logout_form.php"class="btn">logout</a>
-    </div>
+   <div class="content">
+      <h3>hi, <span>manager</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['manager_name'] ?></span></h1>
+      <p>this is an manager page</p>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+   </div>
 
 </div>
      
